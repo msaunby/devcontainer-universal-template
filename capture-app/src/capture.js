e709542b498cc82f8e2af6148802447a4a93d03a
Michael Saunby <mike@saunby.net>
2020-07-24 19:22:29 +0100

    .

--- capture-app/src/capture.js
+++ capture-app/src/capture.js
@@ -1,7 +1,7 @@
 // See https://developer.mozilla.org/en-US/docs/Web/API/Screen_Capture_API/Using_Screen_Capture
 // for the original version of this code with detailed explanation.
 
-export { startCapture, stopCapture};
+export { startCapture, stopCapture };
 
 // Options for getDisplayMedia()
 
@@ -9,8 +9,18 @@ var displayMediaOptions = {
     video: {
         cursor: "always"
     },
-    audio: false
-};
+    audio: true
+    //audio: false
+    //audio: {
+    //    echoCancellation: true,
+    //    noiseSuppression: true,
+    //    sampleRate: 44100
+    //}
+}
+
+let chunks = [];
+
+let mediaRecorder = null;
 
 async function startCapture() {
 
@@ -18,14 +28,46 @@ async function startCapture() {
 
     const videoElem = document.getElementById("video");
     try {
-        videoElem.srcObject = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+        const displayStream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+        videoElem.srcObject = displayStream;
+        mediaRecorder = new MediaRecorder(displayStream, {mimeType: "video/webm"});
+        mediaRecorder.ondataavailable = function(e) {
+            chunks.push(e.data);
+        };
+        mediaRecorder.start();
+        console.log(mediaRecorder.state);
+        console.log("recorder started");
         dumpOptionsInfo();
     } catch (err) {
         console.error("Error: " + err);
     }
 }
 
+function download(data) {
+    var blob = new Blob(data, {
+      type: "video/webm"
+    });
+    var url = URL.createObjectURL(blob);
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    a.href = url;
+    a.download = "test.webm";
+    a.click();
+    window.URL.revokeObjectURL(url);
+  }
+  
+
 function stopCapture() {
+
+    console.log(mediaRecorder.state);
+    console.log(chunks.length);
+    mediaRecorder.stop();
+    console.log(mediaRecorder.state);
+    console.log("recorder stopped");
+
+    download(chunks);
+
     const videoElem = document.getElementById("video");
     let tracks = videoElem.srcObject.getTracks();
 
