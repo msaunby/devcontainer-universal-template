3d7c94e113b51d248a50e96f85292866c8738900
Michael Saunby <mike@saunby.net>
2020-07-25 23:38:21 +0100

    .

--- audio-capture-app/src/App.tsx
+++ audio-capture-app/src/App.tsx
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import { startCapture, stopCapture } from './audio_capture';
+
+const logArray = Array(<></>);
 
 function App() {
+
+
+  // This logging to the page rather than the console is an
+  // interesting trick borrowed from the MDN example (see capture.js),
+  // However it's probably not a good idea to use this regularly.
+  const [log, setLog] = useState(<></>);
+
+  let updateLog = (cl: string, msg: string) => {
+    logArray.push(<span className={cl}>{msg}<br></br></span>);
+    setLog(<>{[...logArray]}</>);
+  }
+
+  console.log = (msg: any) => updateLog("info", msg);
+  console.error = (msg: any) => updateLog("error", msg);
+  console.warn = (msg: any) => updateLog("warn", msg);
+  console.info = (msg: any) => updateLog("info", msg);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello</p>
       </header>
+      <p>This example shows you the contents of the selected part of your display.
+Click the Start Capture button to begin.</p>
+
+      <p><button id="start" onClick={startCapture}>Start Capture</button>&nbsp;<button id="stop" onClick={stopCapture}>Stop Capture</button></p>
+
+      <video id="video" autoPlay></video>
+      <br></br>
+
+      <strong>Log:</strong>
+      <br></br>
+      <pre id="log">{log}</pre>
     </div>
   );
 }
---
+++ audio-capture-app/src/audio_capture.js
@@ -0,0 +1,89 @@
+// See https://developer.mozilla.org/en-US/docs/Web/API/Screen_Capture_API/Using_Screen_Capture
+// for the original version of this code with detailed explanation.
+
+export { startCapture, stopCapture };
+
+// Options for getDisplayMedia()
+
+var displayMediaOptions = {
+    video: {
+        cursor: "always"
+    },
+    audio: true
+    //audio: false
+    //audio: {
+    //    echoCancellation: true,
+    //    noiseSuppression: true,
+    //    sampleRate: 44100
+    //}
+}
+
+let chunks = [];
+
+let mediaRecorder = null;
+
+async function startCapture() {
+
+    console.info("Info: startCapture");
+
+    const videoElem = document.getElementById("video");
+    try {
+        const displayStream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+        videoElem.srcObject = displayStream;
+        mediaRecorder = new MediaRecorder(displayStream, {mimeType: "video/webm"});
+        mediaRecorder.onstop = (e) => {console.log("recorder stopped");};
+        mediaRecorder.ondataavailable = function(e) {
+            chunks.push(e.data);
+        };
+        mediaRecorder.start(1000); // Default behavoiur wasn't working. Slices do.
+        console.log(mediaRecorder.state);
+        console.log("recorder started");
+        dumpOptionsInfo();
+    } catch (err) {
+        console.error("Error: " + err);
+    }
+}
+
+function download(data) {
+    var blob = new Blob(data, {
+      type: "video/webm"
+    });
+    var url = URL.createObjectURL(blob);
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    a.href = url;
+    a.download = "test.webm";
+    a.click();
+    window.URL.revokeObjectURL(url);
+  }
+  
+
+function stopCapture() {
+
+    mediaRecorder.requestData();
+    const videoElem = document.getElementById("video");
+    let tracks = videoElem.srcObject.getTracks();
+
+    tracks.forEach(track => track.stop());
+    videoElem.srcObject = null;
+
+    console.log(mediaRecorder.state);
+    console.log(chunks.length);
+    mediaRecorder.stop();
+    console.log(mediaRecorder.state);
+    console.log("recorder stopped");
+
+    console.log("downloading...");
+    download(chunks);
+}
+
+function dumpOptionsInfo() {
+    const videoElem = document.getElementById("video");
+    const videoTrack = videoElem.srcObject.getVideoTracks()[0];
+
+    console.info("Track settings:");
+    console.info(JSON.stringify(videoTrack.getSettings(), null, 2));
+    console.info("Track constraints:");
+    console.info(JSON.stringify(videoTrack.getConstraints(), null, 2));
+}
