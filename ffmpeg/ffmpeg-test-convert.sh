ee47dca92a16c0a74a4d4e7f95bf7c4519df858e
Michael Saunby <mike@saunby.net>
2020-07-28 20:49:39 +0100

    .

--- ffmpeg/ffmpeg-test-convert.sh
+++ ffmpeg/ffmpeg-test-convert.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-echo "Working dir" $(pwd)
 
-#docker run -v $(pwd):/tmp -w $(pwd)\
-#        jrottenberg/ffmpeg -stats \
-#        -i test.ogg \
-#        test-converted.mp3
+hostdir=/home/mike/github/ExeterBScDTS/devcontainer-universal-template/ffmpeg/samples
 
-docker run -v $(pwd):/tmp \
-        jrottenberg/ffmpeg -stats \
+
+docker run --user 1000:1000 -v ${hostdir}:/tmp/workdir -w /tmp/workdir \
+        jrottenberg/ffmpeg \
         -i test.ogg \
         test-converted.mp3
+
+
+docker run --user 1000:1000 -v ${hostdir}:/tmp/workdir -w /tmp/workdir \
+        jrottenberg/ffmpeg \
+        -i test.ogg -i test.webm \
+        -acodec copy -vcodec copy -f mp4 \
+        test-combined.mp4
